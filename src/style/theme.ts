2e15342c06a90f4d6eee1cd9f187f8a1689b0bcd
ManuCoutinho <gdn.sasha@gmail.com>
2022-02-07 11:00:25 -0300

    :art: style: created theme

---
+++ src/style/theme.ts
@@ -0,0 +1,31 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export const theme = extendTheme({
+  colors: {    
+    gray: {
+      "50": "#F4F4F4",
+      "100": "#EFEFEF",
+      "200":" #DDDDDD",
+      "300": "#969696"
+    },
+    green: {
+      "200": "#94C88B"
+    },
+    orange: {
+      "400":"#ED8E53"
+    },
+
+  },
+  fonts: {
+    body: "Arial",
+    heading: "Arial"
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.100",
+        color: "black",
+      }  
+    }
+  }
+})
